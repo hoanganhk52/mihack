61c7e5777cffd94d946da0916abd226fbd8ff6f6
acv-anhph <anhph@atmarkcafe.org>
2018-06-04 17:58:36 +0700

    complete task 1

--- app.js
+++ app.js
@@ -13,50 +13,25 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/game', function (req, res) {
 	let users = [];
 
-	let user1 = new UserModel({
-		username: req.body.username1
-	});
-	let user2 = new UserModel({
-		username: req.body.username2
-	});
-	let user3 = new UserModel({
-		username: req.body.username3
-	});
-	let user4 = new UserModel({
-		username: req.body.username4
+	req.body.username.forEach((username) => {
+		users.push({username: username});
 	});
 
-	let savegame = async function () {
-		await user1.save().then((user) => {
-			users.push(user._id);
-		});
-		await user2.save().then((user) => {
-			users.push(user._id);
-		});
-		await user3.save().then((user) => {
-			users.push(user._id);
-		});
-		await user4.save().then((user) => {
-			users.push(user._id);
-		});
-
-		let newGame = new GameModel({
-			user: users
-		});
-
-		newGame.save().then((newGame) => {
-			res.redirect(`/game/${newGame._id}`);
-		}).catch((e) => {
-			res.status(400).send(e);
-		});
+	let saveGame = async function () {
+		let savedUsers = await UserModel.create(users);
+		return GameModel.create({user: savedUsers.map(user => user._id)});
 	};
 
-	savegame();
+	saveGame().then(game => {
+		res.redirect(`/game/${game._id}`);
+	}).catch((e) => {
+		res.send(e);
+	});
 });
 
 
--- model/game.js
+++ model/game.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const User = require('./user.js');
 
 const GameSchema = new mongoose.Schema({
-	user: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
-	round: [{userId : {type: String},
-		score: {type: Number, default:0}}]
+	user: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+	round: [
+		[
+			{
+				userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+				score: {type: Number, default: 0}
+			}
+		]
+	]
 });
 
 module.exports = mongoose.model('Game', GameSchema);
